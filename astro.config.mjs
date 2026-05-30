0f3a60191379720e719f45f5a51a2fc1706f6933
Ramy Kaddouri <myramkadd@gmail.com>
2026-05-29 21:28:41 -0700

    setup astro starlight docs site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,35 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightThemeRapidePlugin from 'starlight-theme-rapide';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
-				},
-			],
-		}),
-	],
+  site: 'https://hack4impact-umd.github.io/docs',
+  integrations: [
+    starlight({
+      title: 'H4I-UMD Engineering Docs',
+      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/hack4impact-umd' }],
+      customCss: ['./src/styles/global.css'],
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', slug: 'guides/example' },
+          ],
+        },
+        {
+          label: 'Reference',
+          items: [{ autogenerate: { directory: 'reference' } }],
+        },
+      ],
+      plugins: [starlightThemeRapidePlugin()]
+    }),
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
