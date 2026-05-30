d09f634ce93f53e48fc9864786bdd6cf0314275a
Ramy Kaddouri <myramkadd@gmail.com>
2026-05-29 21:32:14 -0700

    fix base

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://hack4impact-umd.github.io/docs',
+  site: 'https://hack4impact-umd.github.io',
+  base: '/docs',
   integrations: [
     starlight({
       title: 'H4I-UMD Engineering Docs',
